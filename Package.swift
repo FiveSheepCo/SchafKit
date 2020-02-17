5031fce6057730020371ae44152f1ed5505833c9
jannschafranek <jannschafranek@icloud.com>
2020-02-17 15:59:15 +0100

    Change Library Type to Dynamic

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "OpenKit",
+            type: .dynamic,
             targets: ["OpenKit"]),
     ],
     dependencies: [

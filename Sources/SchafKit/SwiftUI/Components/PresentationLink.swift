8cb18befbf211c3569cb06cba4ad0ece13fbea7d
SplittyDev <splittydev@gmail.com>
2020-07-11 13:21:25 +0200

    Add PresentationLink

---
+++ Sources/SchafKit/SwiftUI/Components/PresentationLink.swift
@@ -0,0 +1,46 @@
+//
+//  PresentationLink.swift
+//  
+//
+//  Created by Marco Quinten on 11.07.20.
+//
+
+import SwiftUI
+
+/// Sheet presentation button analogue to NavigationLink.
+struct PresentationLink<Label, Destination>: View where Label: View, Destination: View {
+    @State var isPresented = false
+    
+    var destination: Destination
+    var label: () -> Label
+    var withNavigationView: Bool
+    
+    init(destination: Destination, label: @escaping () -> Label, withNavigationView: Bool = true) {
+        self.destination = destination
+        self.label = label
+        self.withNavigationView = withNavigationView
+    }
+    
+    var body: some View {
+        Button(
+            action: {
+                self.isPresented.toggle()
+            },
+            label: label
+        ).sheet(isPresented: $isPresented) {
+            if self.withNavigationView {
+                NavigationView {
+                    self.destination
+                }
+            } else {
+                self.destination
+            }
+        }
+    }
+}
+
+struct PresentationLink_Previews: PreviewProvider {
+    static var previews: some View {
+        PresentationLink(destination: EmptyView(), label: { Text("PresentationLink") })
+    }
+}

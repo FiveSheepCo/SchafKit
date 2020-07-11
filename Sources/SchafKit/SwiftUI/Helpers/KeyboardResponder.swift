3e37a0ee6c84f6d5e4fdd9e76d8b0a89e48a0455
SplittyDev <splittydev@gmail.com>
2020-07-11 13:21:12 +0200

    Add KeyboardResponder

---
+++ Sources/SchafKit/SwiftUI/Helpers/KeyboardResponder.swift
@@ -0,0 +1,32 @@
+//
+//  KeyboardResponder.swift
+//
+//  Created by Marco Quinten on 23.04.20.
+//
+
+import SwiftUI
+
+final class KeyboardResponder: ObservableObject {
+    private var notificationCenter: NotificationCenter
+    @Published private(set) var currentHeight: CGFloat = 0
+
+    init(center: NotificationCenter = .default) {
+        notificationCenter = center
+        notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    deinit {
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            currentHeight = keyboardSize.height
+        }
+    }
+
+    @objc func keyBoardWillHide(notification: Notification) {
+        currentHeight = 0
+    }
+}

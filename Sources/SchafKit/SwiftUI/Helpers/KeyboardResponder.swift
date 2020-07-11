0d4907795579d13f0a1f3d047654d6b79a2493f9
SplittyDev <splittydev@gmail.com>
2020-07-11 13:57:14 +0200

    Make KeyboardResponder public

--- Sources/SchafKit/SwiftUI/Helpers/KeyboardResponder.swift
+++ Sources/SchafKit/SwiftUI/Helpers/KeyboardResponder.swift
@@ -7,11 +7,11 @@
 import SwiftUI
 
 #if os(iOS)
-final class KeyboardResponder: ObservableObject {
+public final class KeyboardResponder: ObservableObject {
     private var notificationCenter: NotificationCenter
     @Published private(set) var currentHeight: CGFloat = 0
 
-    init(center: NotificationCenter = .default) {
+    public init(center: NotificationCenter = .default) {
         notificationCenter = center
         notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)

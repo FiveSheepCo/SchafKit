626090d283c89f7f328cbdfc73a09b685e642aab
SplittyDev <splittydev@gmail.com>
2020-07-11 13:20:57 +0200

    Add Color+UIColor extensions
    This adds common UIKit colors as SwiftUI Color definitions

---
+++ Sources/SchafKit/Extensions/SwiftUI/Color+UIColor.swift
@@ -0,0 +1,27 @@
+//
+//  Color+UIColor.swift
+//  
+//
+//  Created by Marco Quinten on 11.07.20.
+//
+
+import SwiftUI
+
+public extension Color {
+    static let lightText = Color(UIColor.lightText)
+    static let darkText = Color(UIColor.darkText)
+
+    static let label = Color(UIColor.label)
+    static let secondaryLabel = Color(UIColor.secondaryLabel)
+    static let tertiaryLabel = Color(UIColor.tertiaryLabel)
+    static let quaternaryLabel = Color(UIColor.quaternaryLabel)
+
+    static let systemBackground = Color(UIColor.systemBackground)
+    static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
+    static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
+    
+    static let systemFill = Color(UIColor.systemFill)
+    static let secondarySystemFill = Color(UIColor.secondarySystemFill)
+    static let tertiarySystemFill = Color(UIColor.tertiarySystemFill)
+    static let quaternarySystemFill = Color(UIColor.quaternarySystemFill)
+}

8a6bbc66465ea2a9f73951becd8713f874573eb8
SplittyDev <splittydev@gmail.com>
2020-07-18 16:48:01 +0200

    Add Digest+Hex extension
    Adds Digest.bytes, Digest.data, Digest.hexStr for making working with CryptoKit Digests a lot easier.

---
+++ Sources/SchafKit/Extensions/Other/Digest+Hex.swift
@@ -0,0 +1,20 @@
+//
+//  Digest+Hex.swift
+//  
+//
+//  Created by Marco Quinten on 18.07.20.
+//
+
+#if canImport(CryptoKit)
+import Foundation
+import CryptoKit
+
+extension Digest {
+    var bytes: [UInt8] { Array(makeIterator()) }
+    var data: Data { Data(bytes) }
+
+    var hexStr: String {
+        bytes.map { String(format: "%02X", $0) }.joined()
+    }
+}
+#endif

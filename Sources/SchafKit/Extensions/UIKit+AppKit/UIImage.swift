26daa45c0a31ae5550c91b82d05333622f03e667
Jann Schafranek <me@jannthomas.com>
2020-08-13 19:32:22 +0200

    Fix Color Retrivation

--- Sources/SchafKit/Extensions/UIKit+AppKit/UIImage.swift
+++ Sources/SchafKit/Extensions/UIKit+AppKit/UIImage.swift
@@ -25,6 +25,58 @@
     import UIKit
 #endif
 
+public enum PixelFormat
+{
+    case abgr
+    case argb
+    case bgra
+    case rgba
+}
+
+extension CGBitmapInfo
+{
+    public static var byteOrder16Host: CGBitmapInfo {
+        return CFByteOrderGetCurrent() == Int(CFByteOrderLittleEndian.rawValue) ? .byteOrder16Little : .byteOrder16Big
+    }
+
+    public static var byteOrder32Host: CGBitmapInfo {
+        return CFByteOrderGetCurrent() == Int(CFByteOrderLittleEndian.rawValue) ? .byteOrder32Little : .byteOrder32Big
+    }
+}
+
+extension CGBitmapInfo
+{
+    public var pixelFormat: PixelFormat? {
+
+        // AlphaFirst – the alpha channel is next to the red channel, argb and bgra are both alpha first formats.
+        // AlphaLast – the alpha channel is next to the blue channel, rgba and abgr are both alpha last formats.
+        // LittleEndian – blue comes before red, bgra and abgr are little endian formats.
+        // Little endian ordered pixels are BGR (BGRX, XBGR, BGRA, ABGR, BGR).
+        // BigEndian – red comes before blue, argb and rgba are big endian formats.
+        // Big endian ordered pixels are RGB (XRGB, RGBX, ARGB, RGBA, RGB).
+
+        let alphaInfo: CGImageAlphaInfo? = CGImageAlphaInfo(rawValue: self.rawValue & type(of: self).alphaInfoMask.rawValue)
+        let alphaFirst: Bool = alphaInfo == .premultipliedFirst || alphaInfo == .first || alphaInfo == .noneSkipFirst
+        let alphaLast: Bool = alphaInfo == .premultipliedLast || alphaInfo == .last || alphaInfo == .noneSkipLast
+        let endianLittle: Bool = self.contains(.byteOrder32Little)
+
+        // This is slippery… while byte order host returns little endian, default bytes are stored in big endian
+        // format. Here we just assume if no byte order is given, then simple RGB is used, aka big endian, though…
+
+        if alphaFirst && endianLittle {
+            return .bgra
+        } else if alphaFirst {
+            return .argb
+        } else if alphaLast && endianLittle {
+            return .abgr
+        } else if alphaLast {
+            return .rgba
+        } else {
+            return nil
+        }
+    }
+}
+
 public extension UIImage {
     
     #if os(iOS) || os(tvOS)
@@ -46,7 +98,7 @@ public extension UIImage {
      - Important : The size to reference is the `actualSize` value.
     */
     var colorRepresentations:[[OK8BitRGBARepresentation]]{
-        guard let pixelData = cgImage?.dataProvider?.data else {
+        guard let pixelData = cgImage?.dataProvider?.data, let pixelFormat = cgImage?.bitmapInfo.pixelFormat else {
             return []
         }
         
@@ -62,10 +114,33 @@ public extension UIImage {
             for y in 0..<height {
                 let pixelInfo : Int = ((width * y) + x) * 4
                 
-                let r = data[pixelInfo]
-                let g = data[pixelInfo+1]
-                let b = data[pixelInfo+2]
-                let a = data[pixelInfo+3]
+                let b: UInt8
+                let g: UInt8
+                let r: UInt8
+                let a: UInt8
+                
+                switch pixelFormat {
+                case .abgr:
+                    a = data[pixelInfo]
+                    b = data[pixelInfo+1]
+                    g = data[pixelInfo+2]
+                    r = data[pixelInfo+3]
+                case .argb:
+                    a = data[pixelInfo]
+                    r = data[pixelInfo+1]
+                    g = data[pixelInfo+2]
+                    b = data[pixelInfo+3]
+                case .bgra:
+                    b = data[pixelInfo]
+                    g = data[pixelInfo+1]
+                    r = data[pixelInfo+2]
+                    a = data[pixelInfo+3]
+                case .rgba:
+                    r = data[pixelInfo]
+                    g = data[pixelInfo+1]
+                    b = data[pixelInfo+2]
+                    a = data[pixelInfo+3]
+                }
                 
                 representations.append(OK8BitRGBARepresentation(red: r, green: g, blue: b, alpha: a))
             }

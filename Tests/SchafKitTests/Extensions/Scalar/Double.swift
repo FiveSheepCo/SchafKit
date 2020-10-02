1be436e35749447501ebbf530767f8b8e73db263
Jann Schafranek <me@jannthomas.com>
2020-10-02 13:29:34 +0200

    Fix Double Tests

--- Tests/SchafKitTests/Extensions/Scalar/Double.swift
+++ Tests/SchafKitTests/Extensions/Scalar/Double.swift
@@ -22,6 +22,7 @@
 import XCTest
 
 class DoubleTests : XCTestCase {
+    let locale = Locale(identifier: "en-US")
     
     override func setUp() {
         super.setUp()
@@ -38,13 +39,13 @@ class DoubleTests : XCTestCase {
         
         XCTAssertEqual(double.toString, "1200.75")
         
-        XCTAssertEqual(double.toFormattedString(), "1,200.75")
+        XCTAssertEqual(double.toFormattedString(locale: locale), "1,200.75")
         
-        XCTAssertEqual(double.toFormattedString(decimals: 0), "1,201")
+        XCTAssertEqual(double.toFormattedString(decimals: 0, locale: locale), "1,201")
         
-        XCTAssertEqual(double.toFormattedString(separatesThousands: false), "1200.75")
+        XCTAssertEqual(double.toFormattedString(separatesThousands: false, locale: locale), "1200.75")
         
-        XCTAssertEqual(double.toFormattedString(decimals: 0, separatesThousands: false), "1201")
+        XCTAssertEqual(double.toFormattedString(decimals: 0, separatesThousands: false, locale: locale), "1201")
     }
 }
 #endif

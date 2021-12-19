621a6f0daa6a852ba8eea43124a0016a6b5a652a
JannThomas <jannschafranek@icloud.com>
2021-12-19 17:20:30 +0100

    Add concatenation to NSAttributedString

---
+++ Sources/SchafKit/Extensions/Generic/NSAttributedString.swift
@@ -0,0 +1,17 @@
+//
+//  NSAttributedString.swift
+//  
+//
+//  Created by Jann Schafranek on 19.12.21.
+//
+
+import Foundation
+
+extension NSAttributedString {
+    
+    static func +(lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
+        let new = NSMutableAttributedString(attributedString: lhs)
+        new.append(rhs)
+        return new
+    }
+}

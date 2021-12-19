bfce693c3e7bf1e20b6b4364b179ceb628e00847
JannThomas <jannschafranek@icloud.com>
2021-12-19 17:50:23 +0100

    Fix SchafKit on all Platforms

--- Sources/SchafKit/Kit/SwiftUI/View.swift
+++ Sources/SchafKit/Kit/SwiftUI/View.swift
@@ -31,7 +31,7 @@ public extension View {
             )
     }
     
-    @available(iOS 14.0, *)
+    @available(iOS 14.0, tvOS 14.0, macOS 11.0, watchOS 7.0, *)
     func onAppearAndChange<V>(of value: V, perform action: @escaping (V) -> Void) -> some View where V : Equatable {
         self
             .onAppear {
@@ -40,11 +40,14 @@ public extension View {
             .onChange(of: value, perform: action)
     }
     
+    #if !os(macOS)
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape( RoundedCorner(radius: radius, corners: corners) )
     }
+    #endif
 }
 
+#if !os(macOS)
 private struct RoundedCorner: Shape {
 
     var radius: CGFloat = .infinity
@@ -55,4 +58,25 @@ private struct RoundedCorner: Shape {
         return Path(path.cgPath)
     }
 }
+#endif
 
+// This can be used to make RoundedCorners work on macOS when required. NSBezierPath doesn't support this though, so it would have to be drawn by hand. 🙄
+//#if os(macOS)
+//private typealias UIBezierPath = NSBezierPath
+//public struct UIRectCorner: OptionSet {
+//    public let rawValue: UInt
+//
+//    public init(rawValue: UInt) {
+//        self.rawValue = rawValue
+//    }
+//
+//    public static let topLeft      = UIRectCorner(rawValue: 1 << 0)
+//    public static let topRight     = UIRectCorner(rawValue: 1 << 1)
+//    public static let bottomLeft   = UIRectCorner(rawValue: 1 << 2)
+//    public static let bottomRight  = UIRectCorner(rawValue: 1 << 3)
+//
+//    public static var allCorners: UIRectCorner {
+//        [.topLeft, .topRight, .bottomLeft, .bottomRight]
+//    }
+//}
+//#endif

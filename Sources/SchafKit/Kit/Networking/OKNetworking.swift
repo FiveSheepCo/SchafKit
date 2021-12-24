e64768729336f4a689981aaa0cedbd68a1667c3c
JannThomas <jannschafranek@icloud.com>
2021-12-24 12:29:03 +0100

    Add OKNetworking.request async function

--- Sources/SchafKit/Kit/Networking/OKNetworking.swift
+++ Sources/SchafKit/Kit/Networking/OKNetworking.swift
@@ -24,17 +24,33 @@ import Foundation
 public class OKNetworking {
     
     /// Makes a network request with the given url and options and calls the completion handler when finished.
-    public class func request(url : String,
-                               options : OKOptionSet<OKNetworking.Request.Options> = [],
-                               completion: @escaping RequestCompletionBlock) {
+    public class func request(
+        url : String,
+        options : OKOptionSet<OKNetworking.Request.Options> = []
+    ) async throws -> OKNetworking.RequestResult {
+        try await withCheckedThrowingContinuation({ completion in
+            _Helper.shared.request(url: url, options: options, completion: {
+                completion.resume(with: $0)
+            })
+        })
+    }
+    
+    /// Makes a network request with the given url and options and calls the completion handler when finished.
+    public class func request(
+        url : String,
+        options : OKOptionSet<OKNetworking.Request.Options> = [],
+        completion: @escaping RequestCompletionBlock
+    ) {
         _Helper.shared.request(url: url, options: options, completion: completion)
     }
     
     /// Makes a network download request with the given url and options and calls the completion handler when finished.
-    public class func requestDownload(url : String,
-                               options : OKOptionSet<OKNetworking.Request.Options> = [],
-                               update : @escaping DownloadRequestUpdateBlock = {_,_ in },
-                               completion: @escaping DownloadRequestCompletionBlock) {
+    public class func requestDownload(
+        url : String,
+        options : OKOptionSet<OKNetworking.Request.Options> = [],
+        update : @escaping DownloadRequestUpdateBlock = {_,_ in },
+        completion: @escaping DownloadRequestCompletionBlock
+    ) {
         _Helper.shared.requestDownload(url: url, options: options, update: update, completion: completion)
     }
 }

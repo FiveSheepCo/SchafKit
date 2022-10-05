6b617e26171353652e769454d95c2f22e27b0fdb
Jann Schafranek <me@jannthomas.com>
2022-10-05 17:27:55 +0200

    Add discardableResult to `scheduledTimer` function

--- Sources/SchafKit/Extensions/Generic/Timer.swift
+++ Sources/SchafKit/Extensions/Generic/Timer.swift
@@ -5,7 +5,8 @@ public extension Timer {
     
     @available(tvOS 10.0, *)
     @available(OSX 10.12, *)
-    static func scheduledTimer(withTimeInterval timeInterval : TimeInterval, block : @escaping SKBlock) {
+    @discardableResult
+    static func scheduledTimer(withTimeInterval timeInterval : TimeInterval, block : @escaping SKBlock) -> Timer {
         scheduledTimer(withTimeInterval: timeInterval, repeats: false, block: { (_) in
             block()
         })
